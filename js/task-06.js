7586c3739a6c2ffd10dbdea3ea2b3011ed172c3e
TetianaMyslynska <tetmyslynska@gmail.com>
2021-09-29 11:35:40 +0200

    changed dz7-n.6

--- js/task-06.js
+++ js/task-06.js
@@ -2,10 +2,16 @@ const inputEl = document.querySelector("#validation-input");
 const dataLength = document.querySelector('[data-length="6"]');
 
 inputEl.addEventListener("blur", onInputBlur);
+
+function addClassInput(a, r) {
+  inputEl.classList.add(a);
+  inputEl.classList.remove(r);
+}
 function onInputBlur(event) {
-  if (event.target.value == dataLength) {
-    inputEl.classList.add("valid");
+  const inputText = event.target.value;
+  if (inputText.length == dataLength) {
+    addClassInput("valid", "invalid");
   } else {
-    inputEl.classList.add("invalid");
+    addClassInput("invalid", "valid");
   }
 }
